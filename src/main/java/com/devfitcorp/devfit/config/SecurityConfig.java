68ade60c4071cb20e6953782d7bf0f1399f7e548
muliroZ <murilomega100@gmail.com>
2025-11-15 15:22:48 -0300

    estrutura do front inicial

--- src/main/java/com/devfitcorp/devfit/config/SecurityConfig.java
+++ src/main/java/com/devfitcorp/devfit/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/index.html", "/assets/**", "/css/**", "/js/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
