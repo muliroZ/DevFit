daf185b777a6adc3299cfe8735b1f085158771c2
muliroZ <murilomega100@gmail.com>
2025-11-14 21:47:48 -0300

    merge remoto + local

---
+++ src/main/java/com/devfitcorp/devfit/DevFitApplication.java
@@ -0,0 +1,13 @@
+package com.devfitcorp.devfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevFitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devfitcorp/devfit/DevFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfitcorp.devfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
