5664ce0fbc35bb27f817e5bd94beeb253a516df3
muliroZ <murilomega100@gmail.com>
2025-11-16 13:48:29 -0300

    frontend inicial de autenticação
    Forms de login, cadastro (aluno) e links no index.html

---
+++ src/main/resources/static/js/cadastro.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('register-form');
+    const errorMsg = document.getElementById('error-message');
+
+    form.addEventListener('submit', cadastro)
+
+    async function cadastro(event) {
+        event.preventDefault();
+        
+        const username = document.getElementById('username').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('confirm-password').value;
+
+        if (password !== confirmPassword) {
+            errorMsg.textContent = 'As senhas não batem.'
+            return
+        }
+
+        const data = {
+            username,
+            email,
+            password
+        }
+
+        try {
+            const response = await fetch('/auth/cadastro', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            })
+
+            if (!response.ok) {
+                errorMsg.textContent = 'Erro ao realizar cadastro.'
+                return
+            }
+
+            window.location.href = '../login.html'
+        } catch (error) {
+            console.error('Erro: ', error)
+            errorMsg.textContent = 'Erro ao conectar com o servidor.'
+        }
+    }
+})
