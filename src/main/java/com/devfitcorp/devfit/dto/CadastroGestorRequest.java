e80c191166093c101b53bba1ab798e74684368e6
muliroZ <murilomega100@gmail.com>
2025-11-21 17:04:45 -0300

    Melhoria nos DTOs de cadastro
    Atualizei as validações de campos dos DTOs de cadastro (NotNull -> NotBlank, Valid -> Email, e @ Size para as senhas). Criei uma interface genérica para padronizar parâmetros e flexibilizar o uso de métodos de cadastro.

--- src/main/java/com/devfitcorp/devfit/dto/CadastroGestorRequest.java
+++ src/main/java/com/devfitcorp/devfit/dto/CadastroGestorRequest.java
@@ -1,11 +1,20 @@
 package com.devfitcorp.devfit.dto;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CadastroGestorRequest(
-        @NotNull(message = "O nome de usuário é obrigatório") String nome,
-        @NotNull(message = "A senha é obrigatória") String senha,
-        @NotNull(message = "O email é obrigatório") @Valid String email,
-        @NotNull(message = "Código de administrador obrigatório") String gestorCode
-) {}
+        @NotBlank(message = "O nome de usuário é obrigatório") String nome,
+        @NotBlank(message = "A senha é obrigatória") @Size(min = 8) String senha,
+        @NotBlank(message = "O email é obrigatório") @Email(message = "Formato de email inválido") String email,
+        @NotBlank(message = "Código de administrador obrigatório") String gestorCode
+) implements CadastroBase {
+
+    @Override
+    public void validarCodigo(String codigo) {
+        if (!gestorCode.equals(codigo)) {
+            throw new RuntimeException(); // placeholder
+        }
+    }
+}
