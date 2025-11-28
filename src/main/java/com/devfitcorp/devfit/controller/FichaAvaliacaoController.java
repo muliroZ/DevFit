9a0223c2ee9d04da64aef30c84611c5f41ad571d
Severoabreu <severo.abreu@upe.br>
2025-11-28 17:25:52 -0300

    adicionei o @PreAutrorize no controller. Como recomendado

--- src/main/java/com/devfitcorp/devfit/controller/FichaAvaliacaoController.java
+++ src/main/java/com/devfitcorp/devfit/controller/FichaAvaliacaoController.java
@@ -3,9 +3,13 @@ package com.devfitcorp.devfit.controller;
 
 import com.devfitcorp.devfit.dto.FichaAvaliacaoRequest;
 import com.devfitcorp.devfit.dto.FichaAvaliacaoResponse;
+import com.devfitcorp.devfit.model.Usuario;
 import com.devfitcorp.devfit.service.FichaAvaliacaoService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,31 +26,46 @@ public class FichaAvaliacaoController {
 
     @PostMapping("/criar")
     @ResponseStatus(HttpStatus.CREATED) // Retorna código 201
+    @PreAuthorize("hasAnyROole('Instrutor','Gerente')") // Apenas instrutores e admins podem criar fichas de avaliação
     public FichaAvaliacaoResponse createFichaAvaliacao(@Valid @RequestBody FichaAvaliacaoRequest dto) {
         return  fichaAvaliacaoService.criar(dto);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('Instrutor','Gerente')")
     public List<FichaAvaliacaoResponse> getFichaAvaliacaoByIf(@PathVariable Long id) {
         return fichaAvaliacaoService.buscarPorId(id);
     }
 
     @GetMapping("/aluno/{alunoId}")
+    @PreAuthorize("hasAnyRole('Instrutor','Gerente')")
     public List<FichaAvaliacaoResponse>  getFichasByAluno(@PathVariable Long id) {
         return fichaAvaliacaoService.buscarPorId(id);
     }
+    // mono metodo para o aluno ver suas proprias avaliacoes
+    @GetMapping("/minhas-avaliacoes")
+    @PreAuthorize("hasRole('Aluno')")
+    public List<FichaAvaliacaoResponse> listarMinhasAvaliacoes() {
+        Usuario UsuarioAutenticado = (Usuario) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long alunoId = UsuarioAutenticado.getId();
+        return fichaAvaliacaoService.buscarPorId(alunoId);
+    }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('Instrutor','Gerente')")
     public List<FichaAvaliacaoResponse> getAllFichas() {
         return fichaAvaliacaoService.listar();
     }
 
     @PutMapping("/atualizar/{id}")
+    @PreAuthorize(("hasAnyRole('Instrutor''Gerente')"))
     public FichaAvaliacaoResponse updateFichaAvaliacao(@PathVariable Long id, @Valid @RequestBody FichaAvaliacaoRequest dto) {
         return fichaAvaliacaoService.atualizar(id, dto);
     }
+
     @DeleteMapping("/excluir/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT) // Retorna código 204
+    @ResponseStatus(HttpStatus.NO_CONTENT)// Retorna código 204
+    @PreAuthorize(("hasRole('Gerente')")) // Apenas Gerente podem deletar
     public void deleteFichaAvalicao(@PathVariable Long id) {
         fichaAvaliacaoService.deletar(id);
     }
--- src/main/java/com/devfitcorp/devfit/controller/FichaTreinoController.java
+++ src/main/java/com/devfitcorp/devfit/controller/FichaTreinoController.java
@@ -2,10 +2,13 @@ package com.devfitcorp.devfit.controller;
 
 import com.devfitcorp.devfit.dto.FichaTreinoRequest;
 import com.devfitcorp.devfit.dto.FichaTreinoResponse;
+import com.devfitcorp.devfit.model.Usuario;
 import com.devfitcorp.devfit.service.FichaTreinoService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,6 +25,7 @@ public class FichaTreinoController {
 
     // CRIAR FICHA DE TREINO
     @PostMapping
+    @PreAuthorize("hasAnyRole('GERENTE', 'INSTRUTOR')")
     public ResponseEntity<FichaTreinoResponse> criar(@RequestBody @Valid FichaTreinoRequest request) {
         FichaTreinoResponse response = service.criar(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
@@ -29,18 +33,24 @@ public class FichaTreinoController {
 
     // LISTAR TODAS AS FICHAS
     @GetMapping
+    @PreAuthorize("hasAnyRole('Instrutor','Gerente')")
     public ResponseEntity<List<FichaTreinoResponse>> listar() {
         return ResponseEntity.ok(service.listar());
     }
 
     // BUSCAR POR ID
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('Instrutor','Gerente')")
     public ResponseEntity<FichaTreinoResponse> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(service.buscarPorId(id));
     }
 
+    // falta por o metodo para o aluno ver suas proprias fichas de treino
+    // tentei colocar e constou um erro, vou deixar comentado por enquanto
+
     // ATUALIZAR FICHA
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('GERENTE', 'INSTRUTOR')")
     public ResponseEntity<FichaTreinoResponse> atualizar(
             @PathVariable Long id,
             @RequestBody @Valid FichaTreinoRequest request) {
@@ -52,6 +62,7 @@ public class FichaTreinoController {
 
     // DELETAR FICHA
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('GERENTE')")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         service.deletar(id);
         return ResponseEntity.noContent().build();
