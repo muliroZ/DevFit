f33356d7d5eb43b4913f532094fbea3accfb2c1a
muliroZ <murilomega100@gmail.com>
2025-12-03 16:40:22 -0300

    Implementação da parte de planos de assinatura (backend e frontend)

---
+++ src/main/java/com/devfitcorp/devfit/controller/PlanoController.java
@@ -0,0 +1,25 @@
+package com.devfitcorp.devfit.controller;
+
+import com.devfitcorp.devfit.model.Plano;
+import com.devfitcorp.devfit.service.PlanoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/planos")
+public class PlanoController {
+
+    private final PlanoService planoService;
+
+    public PlanoController(PlanoService planoService) {
+        this.planoService = planoService;
+    }
+
+    @GetMapping
+    public List<Plano> listarAtivos() {
+        return planoService.listarAtivos();
+    }
+}
