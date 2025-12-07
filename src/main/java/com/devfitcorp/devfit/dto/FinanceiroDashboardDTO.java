e7bbc47ab29e3885c58575416cbfa41ba27aa902
muliroZ <murilomega100@gmail.com>
2025-12-07 14:55:09 -0300

    classe -> record: FinanceiroDashboardDTO

--- src/main/java/com/devfitcorp/devfit/dto/FinanceiroDashboardDTO.java
+++ src/main/java/com/devfitcorp/devfit/dto/FinanceiroDashboardDTO.java
@@ -1,22 +1,13 @@
 package com.devfitcorp.devfit.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import java.util.Map;
 import java.math.BigDecimal;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class FinanceiroDashboardDTO {
+public record FinanceiroDashboardDTO(
+        BigDecimal receitaTotal,
+        BigDecimal despesaTotal,
+        BigDecimal lucroLiquido,
 
-    private BigDecimal receitaTotal;
-    private BigDecimal despesaTotal;
-    private BigDecimal lucroLiquido;
-
-    private Map<String, BigDecimal> receitaPorFonte;
-    private Map<String, BigDecimal> despesaPorCategoria;
-}
+        Map<String, BigDecimal> receitaPorFonte,
+        Map<String, BigDecimal> despesaPorCategoria
+) {}
